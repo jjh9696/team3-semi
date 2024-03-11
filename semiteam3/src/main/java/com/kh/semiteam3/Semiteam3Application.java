1ec1319d1d4d1b912dca21356e60628cb1bac97d
SoyeonHyung <gudskirjs@naver.com>
2024-03-11 09:52:31 +0900

    (세미프로젝트 준비)semiteam3 프로젝트 생성

---
+++ semiteam3/src/main/java/com/kh/semiteam3/Semiteam3Application.java
@@ -0,0 +1,13 @@
+package com.kh.semiteam3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Semiteam3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Semiteam3Application.class, args);
+	}
+
+}
---
+++ semiteam3/src/test/java/com/kh/semiteam3/Semiteam3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.semiteam3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Semiteam3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

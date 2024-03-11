01289ed496f5d70d3f73bdd8f5b5ae75696dd6d3
dbwjd727 <ppap030727@naver.com>
2024-03-11 15:23:22 +0900

    attach dto,mapper,dao

---
+++ semiteam3/src/main/java/com/kh/semiteam3/dao/AttachDao.java
@@ -0,0 +1,68 @@
+package com.kh.semiteam3.dao;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.kh.semiteam3.dto.AttachDto;
+import com.kh.semiteam3.mapper.AttachMapper;
+
+@Repository
+public class AttachDao {
+
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	@Autowired
+	private AttachMapper attachMapper;
+	
+	//시퀀스 값 가져오기
+	public int getSequence() {
+		String sql = "select attach_seq.nextval from dual";
+		return jdbcTemplate.queryForObject(sql, int.class);
+	}
+	
+	//등록
+	public void insert(AttachDto attachDto) {
+		String sql = "insert into attach ("
+				+ "attach_no, attach_name, attach_type, attach_size"
+				+ ") values(?, ?, ?, ?)";
+		Object[] data = {
+				attachDto.getAttachNo(), attachDto.getAttachName(),
+				attachDto.getAttachType(), attachDto.getAttachSize()
+		};
+		jdbcTemplate.update(sql, data);
+	}
+	
+	//검색
+	public AttachDto selectOne(int attachNo) {
+		String sql = "select * from attach where attach_no = ?";
+		Object[] data = {attachNo};
+		List<AttachDto> list = jdbcTemplate.query(sql, attachMapper, data);
+		return list.isEmpty() ? null : list.get(0);
+	}
+
+	//삭제
+	public boolean delete(int attachNo) {
+		String sql = "delete attach where attach_no = ?";
+		Object[] data = {attachNo};
+		return jdbcTemplate.update(sql, data) > 0;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
